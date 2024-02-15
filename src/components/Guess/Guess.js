71d2938f0408bd35f48662552a240d7b6b2f275f
steve <steverios_sr@yahoo.com>
2024-02-14 20:23:13 -0600

    Guess:started and wired in

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Guess({ guess }) {
+  return (
+    <p className="guess">
+      <span className="cell">H</span>
+      <span className="cell">E</span>
+      <span className="cell">L</span>
+      <span className="cell">L</span>
+      <span className="cell">O</span>
+    </p>
+  );
+}
+
+export default Guess;
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -4,8 +4,47 @@ function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
       {guesses.map((guess, index) => (
-        <p key={index} className="guess">{guess}</p>
+        <p key={index} className="guess">
+          {guess}
+        </p>
       ))}
+      <div class="guess-results">
+        <p class="guess">
+          <span class="cell">H</span>
+          <span class="cell">E</span>
+          <span class="cell">L</span>
+          <span class="cell">L</span>
+          <span class="cell">O</span>
+        </p>
+        <p class="guess">
+          <span class="cell">T</span>
+          <span class="cell">H</span>
+          <span class="cell">E</span>
+          <span class="cell">R</span>
+          <span class="cell">E</span>
+        </p>
+        <p class="guess">
+          <span class="cell">W</span>
+          <span class="cell">O</span>
+          <span class="cell">R</span>
+          <span class="cell">L</span>
+          <span class="cell">D</span>
+        </p>
+        <p class="guess">
+          <span class="cell"></span>
+          <span class="cell"></span>
+          <span class="cell"></span>
+          <span class="cell"></span>
+          <span class="cell"></span>
+        </p>
+        <p class="guess">
+          <span class="cell"></span>
+          <span class="cell"></span>
+          <span class="cell"></span>
+          <span class="cell"></span>
+          <span class="cell"></span>
+        </p>
+      </div>
     </div>
   );
 }
